9c6214b3fecd3f722ed3564672e2558d96d537af
jkaagh <69788622+jkaagh@users.noreply.github.com>
2021-04-23 12:50:48 +0200

    jessu fuck ingssfdasd

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,10 @@ db.once("open", () => console.log("Connected to database"))
 const app = express();
 const server = http.createServer(app)
 
+var allowedOrigins = "http://localhost:* http://127.0.0.1:*";
 
 //i copy pasted this, i dont know what it does
-const whitelist = ['http://localhost:3001'];
+const whitelist = ['http://localhost:3000'];
 const corsOptions = {
   credentials: true, // This is important.
   origin: (origin, callback) => {
@@ -32,13 +33,21 @@ const corsOptions = {
 
 //what is this for?
 app.use(express.json())
+// app.use(cors({ credentials: true, origin: true }))
 app.use(cors(corsOptions))
+
 const options = {
     cors:true,
-    origins:["http://localhost:3001"]
+    origins:["http://localhost:3000"]
 };
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server,{
+    origins: allowedOrigins,
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
 
 
 //enable me to use routes in another file.
@@ -48,6 +57,7 @@ app.use("/", Routes)
 //when a connection is established with any client
 io.on("connection", socket =>{
     console.log("New WS Connection...")
+    
 })
 
 //boot up server on specified port and run a log.
