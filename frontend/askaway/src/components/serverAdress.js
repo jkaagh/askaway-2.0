42db752dafc44c12ec363c5f410e025838645039
jkaagh <69788622+jkaagh@users.noreply.github.com>
2021-06-14 15:11:07 +0200

    asdasd

--- frontend/askaway/src/components/serverAdress.js
+++ frontend/askaway/src/components/serverAdress.js
@@ -6,7 +6,7 @@ const address = {
 }
 
 if(online){
-    address.address = "https://askawayapp.heroku.com"
+    address.address = "https://askawayapp.herokuapp.com"
 }
 else{
     address.address = "http://localhost:3001"
